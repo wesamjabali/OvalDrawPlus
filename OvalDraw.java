5884a744774789e1a84fc224eac16760a8ec45f8
Wesam Jabali <wesam@Wesams-MacBook-Pro.local>
2019-02-07 15:06:17 -0600

    Initial file

--- OvalDraw.java
+++ OvalDraw.java
@@ -1,19 +1,47 @@
 import javax.swing.JFrame;
+import javac.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Container;
+import java.awt.Color;
+import java.awt.Component;
+
+class Oval extends JPanel {
+    Oval() {
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // g.fillRect(0, 100, 100, 100);
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        //Color rectColor = new Color(0,255,0);
+        //g.fillRect(0,0,panelWidth, panelHeight);
+
+        Color myColor = new Color(255,0,0);
+        g.setColor(myColor);
+
+        g.fillOval(0, 0, 100, 100);
+    }
+}
 
 class OvalFrame extends JFrame {
     OvalFrame() {
-        setTitle("OvalDraw");
-        setBounds(200,200,300,400);
+        setBounds(300, 300, 300, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+        setTitle("OvalDraw");
 
+        Oval myOval = new Oval();
+        Container contentPane = getContentPane();
+        contentPane.add(myOval);
+    }
 }
 
 public class OvalDraw {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("OvalDraw Starting...");
         OvalFrame myFrame = new OvalFrame();
-
         myFrame.setVisible(true);
     }
 }
